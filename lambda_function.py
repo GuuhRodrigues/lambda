8f65533836ae9a25ab32cc6383e618154ab8c4e3
Gustavo Rodrigues <guhrodriguesads@gmail.com>
2023-07-27 23:07:34 -0300

    Alterando log

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 from log import function_log
 
 def lambda_handler(event, context):
-    function_log('Log de execução '+ json.dumps(event))
+    function_log('Log de execução após GitHub Action '+ json.dumps(event))
     return {
         'statusCode': 200,
         'body': json.dumps(event)
